d64676e6d9d5f12fdf8558fa93c99f55df0f1ae4
TabindaNoor <tabindanoor415@gmail.com>
2023-11-17 21:26:30 +0500

    done with it

--- src/App.js
+++ src/App.js
@@ -1,25 +1,123 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from 'react';
+import Tesseract from 'tesseract.js';
+import styled from 'styled-components';
+import VideoEditor from 'react-html5-video-editor';
 
-function App() {
+
+const Container = styled.div`
+  max-width: 600px;
+  margin: auto;
+  text-align: center;
+  padding: 20px;
+`;
+
+const Input = styled.input`
+  display: none;
+`;
+
+const Label = styled.label`
+  padding: 10px;
+  margin: 10px;
+  cursor: pointer;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+`;
+
+const Button = styled.button`
+  padding: 10px;
+  margin: 10px;
+  cursor: pointer;
+`;
+
+const App = () => {
+  const inputFileRef = useRef(null);
+  const [imageSrc, setImageSrc] = useState('');
+  const [textFromImage, setTextFromImage] = useState('');
+  const [error, setError] = useState('');
+  const [isVideoEditorOpen, setIsVideoEditorOpen] = useState(false);
+
+  const handleFileChange = async (event) => {
+    const file = event.target.files[0];
+
+    if (file) {
+      try {
+        const imageSrc = URL.createObjectURL(file);
+        setImageSrc(imageSrc);
+        convertImageToText(imageSrc);
+      } catch (error) {
+        setError('Error loading the image. Please try again.');
+      }
+    }
+  };
+
+  const convertImageToText = async (imageSrc) => {
+    try {
+      const { data: { text } } = await Tesseract.recognize(imageSrc, 'eng', {
+        logger: info => console.log(info),
+      });
+
+      setTextFromImage(text);
+      setError('');
+    } catch (error) {
+      setError('Error processing image. Please try again.');
+    }
+  };
+
+  const speakText = () => {
+    if (textFromImage) {
+      const synth = window.speechSynthesis;
+      const utterance = new SpeechSynthesisUtterance(textFromImage);
+      synth.speak(utterance);
+    }
+  };
+
+  const handleVideoEditorClose = () => {
+    setIsVideoEditorOpen(false);
+  };
+
+  const handleGenerateVideo = () => {
+    setIsVideoEditorOpen(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <h1>Image to Text</h1>
+
+      <Input
+        type="file"
+        accept="image/*"
+        ref={inputFileRef}
+        onChange={handleFileChange}
+      />
+      <Label onClick={() => inputFileRef.current.click()}>Choose Image</Label>
+
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+
+      {imageSrc && (
+        <div>
+          <img src={imageSrc} alt="Selected" style={{ maxWidth: '100%' }} />
+
+          {textFromImage && (
+            <div>
+              <h2>Text from Image:</h2>
+              <p>{textFromImage}</p>
+
+              <Button onClick={speakText}>Speak Text</Button>
+
+              <Button onClick={handleGenerateVideo}>Generate Video</Button>
+            </div>
+          )}
+
+{isVideoEditorOpen && (
+            <VideoEditor
+              text={textFromImage}
+              onVideoEditClose={handleVideoEditorClose}
+            />
+          )}
+        </div>
+      )}
+    </Container>
   );
-}
+};
 
 export default App;
